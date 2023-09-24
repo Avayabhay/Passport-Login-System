540ff27e4ea8c816c46b0c3cc23a76032484efdf
Abhay Sharma <avay.abhay@gmail.com>
2023-09-24 23:31:31 +0530

    Created new branch for refactored code
    Restarted doing the project from begining and following other tutorials

--- server.js
+++ server.js
@@ -3,72 +3,108 @@ if (process.env.NODE_ENV !== "production") {
   // console.log("it entered");
 }
 
+const mongoURI = "mongodb://0.0.0.0:27017/mySession";
+//const { MongoDBStore } = require("connect-mongodb-session");
 const express = require("express");
 const app = express();
-const bcrypt = require("bcrypt");
-const passport = require("passport");
-const flash = require("express-flash");
+const mongoose = require("mongoose");
+
+// const bcrypt = require("bcrypt");
+// const passport = require("passport");
+// const flash = require("express-flash");
 const session = require("express-session");
 
-const users = [];
+mongoose
+  .connect(mongoURI, {
+    useNewUrlParser: true,
+    // useCreateIndex: true,
+    useUnifiedTopology: true,
+  })
+  .then((res) => {
+    console.log("db connected");
+  })
+  .catch((err) => console.log(err));
 
-const getUserByEmail = (email) => users.find((user) => user.email === email);
-
-const initializePass = require("./passport.config");
-initializePass(passport, getUserByEmail, (id) =>
-  users.find((user) => user.id === id)
+const MongoDBStore = require("connect-mongodb-session")(session);
+const store = new MongoDBStore(
+  {
+    uri: mongoURI,
+    //collection: "mySessions",
+  },
+  function (error) {
+    // Should have gotten an error
+    console.log(error);
+  }
 );
 
-app.set("view-engin", "ejs");
-app.use(express.urlencoded({ extended: false }));
-app.use(flash());
+store.on("error", function (error) {
+  console.log(error);
+});
+// const users = [];
+
+// const getUserByEmail = (email) => users.find((user) => user.email === email);
+
+// const initializePass = require("./passport.config");
+// initializePass(passport, getUserByEmail, (id) =>
+//   users.find((user) => user.id === id)
+// );
+
+// app.set("view-engin", "ejs");
+// app.use(express.urlencoded({ extended: false }));
+// app.use(flash());
 app.use(
   session({
-    secret: process.env.SESSION_SECRET,
+    secret: "process",
     resave: false,
     saveUninitialized: false,
+    cookie: {
+      maxAge: 20 * 1000,
+    },
+    store: store,
   })
 );
-app.use(passport.initialize());
-app.use(passport.session());
+// app.use(passport.initialize());
+// app.use(passport.session());
 
 app.get("/", (req, res) => {
-  //   res.send("Passport Login System");
-  res.render("index.ejs", { name: req.user.name });
+  req.session.isAuth = true;
+  console.log(req.session);
+  res.send("Passport Login System  !");
+  // res.render("index.ejs", { name: req.user.name });
 });
 
-app.get("/login", (req, res) => {
-  res.render("login.ejs");
-});
+// app.get("/login", (req, res) => {
+//   res.render("login.ejs");
+// });
 
-app.get("/register", (req, res) => {
-  res.render("register.ejs");
-});
+// app.get("/register", (req, res) => {
+//   res.render("register.ejs");
+// });
 
-app.post("/register", async (req, res) => {
-  try {
-    const hashedPass = await bcrypt.hash(req.body.pass, 10);
-    console.log(hashedPass);
-    users.push({
-      id: Date.now().toString(),
-      name: req.body.name,
-      email: req.body.email,
-      pass: hashedPass,
-    });
-    console.log(users);
-    res.redirect("/login");
-  } catch {
-    res.redirect("/register");
-  }
-  // console.log(user);
-});
+// app.post("/register", async (req, res) => {
+//   try {
+//     const hashedPass = await bcrypt.hash(req.body.pass, 10);
+//     console.log(hashedPass);
+//     users.push({
+//       id: Date.now().toString(),
+//       name: req.body.name,
+//       email: req.body.email,
+//       pass: hashedPass,
+//     });
+//     console.log(users);
+//     res.redirect("/login");
+//   } catch {
+//     res.redirect("/register");
+//   }
+//   // console.log(user);
+// });
 
-app.post(
-  "/login",
-  passport.authenticate("local", {
-    successRedirect: "/",
-    failureRedirect: "/login",
-    failureFlash: true,
-  })
-);
+// app.post(
+//   "/login",
+//   passport.authenticate("local", {
+//     successRedirect: "/",
+//     failureRedirect: "/login",
+//     failureFlash: true,
+//   })
+// );
 app.listen(3000);
